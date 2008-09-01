96d7b481c3d42839eff0943cc90b1cdb83bc5238
Orestis Markou <orestis@orestis.gr>
2008-09-01 02:27:56 +0100

    Added failing functional tests for package handling

--- functional_test.py
+++ functional_test.py
@@ -6,6 +6,59 @@ import os
 import pysmelltags
 
 class FunctionalTest(unittest.TestCase):
+    def setUp(self):
+        self.packageA = {
+            'PackageA.ModuleA': {
+                'FUNCTIONS': [('TopLevelFunction', ['arg1', 'arg2'], "")],
+                'CONSTANTS': ['CONSTANT',],
+                'CLASSES':
+                    {'ClassA': {
+                            'bases': ['object'],
+                            'docstring': '',
+                            'constructor': [],
+                            'properties': ['classPropertyA', 'classPropertyB', 'propertyA', 'propertyB', 'propertyC', 'propertyD'],
+                            'methods': [('methodA', ['argA', 'argB', '*args', '**kwargs'], '')]
+                        },
+                     'ChildClassA': {
+                            'bases': ['ClassA', 'object'],
+                            'docstring': 'a class docstring, imagine that',
+                            'constructor': ['conArg'],
+                            'properties': ['extraProperty'],
+                            'methods': [('extraMethod', [], 'i have a docstring')],
+                        }
+                    },
+                },
+            'PackageA': {
+                'FUNCTIONS': [('SneakyFunction'), [], ""],
+                'CONSTANTS': ['SneakyConstant'],
+                'CLASSES':{
+                    'SneakyClass': {
+                            'bases': [],
+                            'docstring': '',
+                            'constructor': '',
+                            'properties': [],
+                            'methods': []
+                            }
+                        }
+                    }
+                }
+        self.packageB = {
+            'PackageB': {
+                'FUNCTIONS': [('SneakyFunction'), [], ""],
+                'CONSTANTS': ['SneakyConstant'],
+                'CLASSES':{
+                    'SneakyClass': {
+                            'bases': [],
+                            'docstring': '',
+                            'constructor': '',
+                            'properties': [],
+                            'methods': []
+                            }
+                        }
+                    }
+                }
+            
+        
     def DONTtestTagGeneration(self):
         input_source = dedent("""\
         "This is MyClass' docstring"
@@ -37,36 +90,25 @@ class FunctionalTest(unittest.TestCase):
         self.assertEquals(output._modules['CLASSES'], expected)
         self.fail('check *args **kwargs')
 
+    def testMultiPackage(self):
+        if os.path.exists('Tests/PYSMELLTAGS'):
+            os.remove('Tests/PYSMELLTAGS')
+        subprocess.call(["python", "../pysmelltags.py", "PackageA", "PackageB"], cwd='Tests')
+        self.assertTrue(os.path.exists('Tests/PYSMELLTAGS'))
+        PYSMELLDICT = eval(open('Tests/PYSMELLTAGS').read())
+        expectedDict = {}
+        expectedDict.update(self.packageA)
+        expectedDict.update(self.packageB)
+        self.assertEquals(PYSMELLDICT, expectedDict)
+
+    
     def testPackage(self):
         if os.path.exists('Tests/PYSMELLTAGS'):
             os.remove('Tests/PYSMELLTAGS')
         subprocess.call(["python", "../pysmelltags.py", "PackageA"], cwd='Tests')
         self.assertTrue(os.path.exists('Tests/PYSMELLTAGS'))
         PYSMELLDICT = eval(open('Tests/PYSMELLTAGS').read())
-        expectedDict = {
-            'ModuleA': {
-                'FUNCTIONS': [('TopLevelFunction', ['arg1', 'arg2'], "")],
-                'CONSTANTS': ['CONSTANT',],
-                'CLASSES':
-                    {'ClassA': {
-                            'bases': ['object'],
-                            'docstring': '',
-                            'constructor': [],
-                            'properties': ['classPropertyA', 'classPropertyB', 'propertyA', 'propertyB', 'propertyC', 'propertyD'],
-                            'methods': [('methodA', ['argA', 'argB', '*args', '**kwargs'], '')]
-                        },
-                     'ChildClassA': {
-                            'bases': ['ClassA', 'object'],
-                            'docstring': 'a class docstring, imagine that',
-                            'constructor': ['conArg'],
-                            'properties': ['extraProperty'],
-                            'methods': [('extraMethod', [], 'i have a docstring')],
-                        }
-                    }
-                
-            }
-                        
-        }
+        expectedDict = self.packageA
         self.assertEquals(PYSMELLDICT, expectedDict)
 
     
