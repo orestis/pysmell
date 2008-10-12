c8a465e86e656d1b7e6275ca418a1a51797b43cd
Orestis Markou <orestis@orestis.gr>
2008-10-12 20:05:23 +0100

    Moved tests into Tests Added functional test to alltests.py

--- alltests.py
+++ alltests.py
@@ -1,15 +1,20 @@
 from unittest import TestLoader, TextTestRunner
 
+import sys
+sys.path.append('Tests')
+
 import test_codefinder
 import test_completions
 import test_vimhelper
 import test_idehelper
 import test_matchers
+import functional_test
 
 suite = TestLoader().loadTestsFromModule(test_codefinder)
 suite.addTest(TestLoader().loadTestsFromModule(test_completions))
 suite.addTest(TestLoader().loadTestsFromModule(test_vimhelper))
 suite.addTest(TestLoader().loadTestsFromModule(test_idehelper))
 suite.addTest(TestLoader().loadTestsFromModule(test_matchers))
+suite.addTest(TestLoader().loadTestsFromModule(functional_test))
 
 TextTestRunner(verbosity=2).run(suite)
